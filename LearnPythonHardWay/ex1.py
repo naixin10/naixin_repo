c3f4d5d31e882f00ad0448104a99c740ee089a06
naixin10 <2848089@qq.com>
2017-03-08 10:55:58 +0800

    add ex1~ex5

---
+++ LearnPythonHardWay/ex1.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+print "Hello World!"
+print "Hello Again"
+print "I like typing this."
+print "This is fun."
+print u'Yay! 你好.'
+print "I'd much rather you 'not'."
+print 'I "said" do not touch this.'
+print "oh,,,git + python"
